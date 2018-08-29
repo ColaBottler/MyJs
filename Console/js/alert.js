40564f76f63d0322274c9a48cf6285a66bc5b5e1
钟懿的帅电脑 <1515686270@qq.com>
2018-08-29 15:17:10 +0800

    modify

--- Console/js/alert.js
+++ Console/js/alert.js
@@ -1,11 +1,11 @@
-var j="再",temp="";
+var j = "再", temp = "";
 alert("有本事你就点一下");
-function Taolu()
-{
-    for(var i=1;;i++)
-    {
-        temp+=j;
-        alert("有本事就"+temp+"点一下");
+
+function Taolu() {
+    for (var i = 1; ; i++) {
+        temp += j;
+        alert("有本事就" + temp + "点一下");
     }
 }
+
 Taolu();
